5189d1b66ef681583d502b1416395e7c1120d7d7
James Douglas <james@phytochemconsulting.ca>
2024-03-04 18:08:25 -0800

    done splash page

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,13 +1,16 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+// import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { FaTree } from "react-icons/fa6";
 
 export default function Logo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">ForestFriends</p>
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <FaTree size={50} />
+
+      <p className="text-[44px]">Forest Friends</p>
     </div>
   );
 }
