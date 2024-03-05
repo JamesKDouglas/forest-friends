4a87d0a7671b0a651b0eccd68849eac082570d06
James Douglas <james@phytochemconsulting.ca>
2024-03-04 18:13:24 -0800

    add favicon

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Logo from '@/app/ui/logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+import { lusitana } from '@/app/ui/fonts'
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-green-700 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <Logo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -21,7 +23,7 @@ export default function Page() {
             </div>
           </div>
 
-          <p className = "text-2xl">
+          <p className = { `${lusitana.className} text-2xl` }>
             <strong>Welcome to Forest Friends.</strong> Summer camp for forest fun!
           </p>
           <Link
@@ -33,6 +35,13 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/LogoTree.png"
+            width={200}
+            height={200}
+            className="hidden md:block"
+            alt="Drawing of a tree. Trees are great, thanks trees!"
+          />
         </div>
       </div>
     </main>
