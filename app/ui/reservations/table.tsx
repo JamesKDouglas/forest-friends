0e39f902078b22dd0a89959a4d0762cc06515d0d
James Douglas <james@phytochemconsulting.ca>
2024-03-07 17:57:22 -0800

    remove image from reservations table. Perhaps it would be better security to have an image of the gaurdian but it is also annoying for them to provide that during signup.

--- app/ui/reservations/table.tsx
+++ app/ui/reservations/table.tsx
@@ -26,13 +26,6 @@ export default async function ReservationsTable({
                 <div className="flex items-center justify-between border-b pb-4">
                   <div>
                     <div className="mb-2 flex items-center">
-                      <Image
-                        src={reservation.image_url}
-                        className="mr-2 rounded-full"
-                        width={28}
-                        height={28}
-                        alt={`${reservation.name}'s profile picture`}
-                      />
                       <p>{reservation.name}</p>
                     </div>
                     <p className="text-sm text-gray-500">{reservation.email}</p>
